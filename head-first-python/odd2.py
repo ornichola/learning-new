c9e7a5c89433f7964a4c910cbae3ee1c3590dbfa
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-25 22:10:15 +0300

    head-first-python ch01 odd2.py

---
+++ head-first-python/odd2.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import time
+import random
+
+odds = [ 1, 3, 5, 7, 9, 13, 15, 17, 19,
+        21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+        41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
+
+for _ in range(5):
+    right_this_minute = datetime.today().minute	
+    if right_this_minute in odds:
+        print("This minute seems a little odd.")
+    else:
+        print("Not and odd minute.")
+    wait_time = random.randint(1,60)
+    time.sleep(wait_time)
