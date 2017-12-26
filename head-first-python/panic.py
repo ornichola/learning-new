cb14e8ffa75b49580e542f2d8c27567357261e36
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-26 13:30:20 +0300

    head-first-python ch02 panic.py adding authors solution

--- head-first-python/panic.py
+++ head-first-python/panic.py
@@ -14,3 +14,13 @@ plist.insert(4, 'a')
 new_phrase = ''.join(plist)
 print(plist)
 print(new_phrase)
+
+'''
+# authors solution
+for i in range(4):
+plist.pop()
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
+'''
