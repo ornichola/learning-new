c09f4734c4e6f66b6bb4fbf5fd3776766bbfcbb4
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-27 20:59:07 +0300

    head-first-python ch04 vsearch.py

--- head-first-python/vsearch.py
+++ head-first-python/vsearch.py
@@ -1,9 +1,10 @@
-def search4vowels(phrase:str) -> set:
+def search4vowels(phrase: str) -> set:
     """Выводит гласные, найденные в указанной фразе."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
-def search4letters(phrase:str, letters:str) -> set:
+
+def search4letters(phrase: str, letters: str) -> set:
     """
     Выводит множество символов, найденных в указанной фразе,
     по переданному паттерну.
