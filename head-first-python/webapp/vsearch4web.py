f2ada3c705531ebd716b443663aa640dba4580d1
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-28 01:44:55 +0300

    head-first-python ch05 webapp/vsearch4web.py replacing redirect by decorator

--- head-first-python/webapp/vsearch4web.py
+++ head-first-python/webapp/vsearch4web.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello() -> '302':
-    return redirect('/entry')
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -19,6 +15,7 @@ def do_search() -> 'html':
                             the_letters=letters,
                             the_results=results,)
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
