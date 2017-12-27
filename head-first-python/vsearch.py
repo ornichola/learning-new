e548a98939fd88738c50e87b07d331b5529e93e2
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-27 16:17:10 +0300

    head-first-python ch04 vsearch.py PEP8 strings

--- head-first-python/vsearch.py
+++ head-first-python/vsearch.py
@@ -1,7 +1,7 @@
 def search4vowels():
     """Выводит гласные, найденные во введенном слове."""
     vowels = set('aeiou')
-    word = input("Provide a word to search for vowels: ")
+    word = input('Provide a word to search for vowels: ')
     found = vowels.intersection(set(word))
     for vowel in found:
         print(vowel)
