6e46002f38bd45962e6c1c293b91ed4008218f8c
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-27 19:28:00 +0300

    head-first-python ch04 vsearch.py

--- head-first-python/vsearch.py
+++ head-first-python/vsearch.py
@@ -1,7 +1,15 @@
-def search4vowels():
-    """Выводит гласные, найденные во введенном слове."""
+def search4vowels(phrase:str) -> set:
+    """Выводит гласные, найденные в указанной фразе."""
     vowels = set('aeiou')
-    word = input('Provide a word to search for vowels: ')
-    found = vowels.intersection(set(word))
-    for vowel in found:
-        print(vowel)
+    return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str, letters:str) -> set:
+    """
+    Выводит множество символов, найденных в указанной фразе,
+    по переданному паттерну.
+    """
+    return set(letters).intersection(set(phrase))
+
+print(search4letters('hitch-hiker', 'aeiou'))
+print(search4letters('galaxy', 'xyz'))
+print(search4letters('life, the universe, and everything', 'o'))
