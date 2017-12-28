b15eb8b342b716b194e2e94534c982a7f59e821f
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-28 18:19:53 +0300

    head-first-python ch06 vsearch4web.py viewlog page is done

--- head-first-python/webapp/vsearch4web.py
+++ head-first-python/webapp/vsearch4web.py
@@ -46,14 +46,18 @@ def view_the_log() -> str:
 '''
 
 @app.route('/viewlog')
-def view_the_log() -> 'str':
+def view_the_log() -> 'html':
     contents = []
     with open('vsearch.log') as log:
         for line in log:
             contents.append([])
             for item in line.split('|'):
                 contents[-1].append(escape(item))
-    return str(contents)
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                            the_title='View Log',
+                            the_row_titles=titles,
+                            the_data=contents,)
 
 
 if __name__ == '__main__':
