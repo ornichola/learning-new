3e84cc01a1652958d9155f6d4595289362c6fa0f
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-29 23:51:20 +0300

    head-first-python ch08 CountFromBy

--- head-first-python/countfromby.py
+++ head-first-python/countfromby.py
@@ -1,4 +1,11 @@
 class CountFromBy:
+
+    def __init__(self, v: int, i: int) -> None:
+        self.val = v
+        self.incr = i
+
     def increase(self) - > None:
         self.val += self.incr
-        
+
+    def __repr__(self) -> str:
+        return str(self.val)
