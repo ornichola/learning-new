32dfae3081e21e5ff3b7aeda511519345a0dbdbb
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-30 02:16:59 +0300

    head-first-python ch09 vsearch4web.py is now using ContextManager

--- head-first-python/webapp/vsearch4web.py
+++ head-first-python/webapp/vsearch4web.py
@@ -1,32 +1,28 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+from DBcm import UseDatabase
+
 
 app = Flask(__name__)
+app.config['dbconfig'] = { 'host': '127.0.0.1',
+            'user': 'vsearch',
+            'password': 'vsearchpasswd',
+            'database': 'vsearchlogDB', }
 
-import mysql.connector
 
 def log_request(req: 'flask_request', res: str) -> None:
     """Журналирует веб-запрос и возвращаемые результаты."""
 
-    dbconfig = { 'host': '127.0.0.1',
-                'user': 'vsearch',
-                'password': 'vsearchpasswd',
-                'database': 'vsearchlogDB', }
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-
-    _SQL = """insert into log
-            (phrase, letters, ip, browser_string, results)
-            values
-            (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'],
-                        req.form['letters'],
-                        req.remote_addr,
-                        req.user_agent.browser,
-                        res, ))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with UseDatabase(dbconfig) as cursor:
+        _SQL = """insert into log
+                (phrase, letters, ip, browser_string, results)
+                values
+                (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                            req.form['letters'],
+                            req.remote_addr,
+                            req.user_agent.browser,
+                            res, ))
 
 
 @app.route('/search4', methods=['POST'])
