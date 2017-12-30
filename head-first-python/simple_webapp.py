d669bc7a5c7968612c8cccca55b4a22a759267e3
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-31 01:11:50 +0300

    head-first-python ch10 simple_webapp.py is now using decorator @check_logged_in

--- head-first-python/simple_webapp.py
+++ head-first-python/simple_webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, session
+from checker import check_logged_in
 
 app = Flask(__name__)
 
@@ -9,14 +10,17 @@ def hello() -> str:
     return 'Hello from the simple webapp.'
 
 @app.route('/page1')
+@check_logged_in
 def page1() -> str:
     return 'This is page 1.'
 
 @app.route('/page2')
+@check_logged_in
 def page2() -> str:
     return 'This is page 2.'
 
 @app.route('/page3')
+@check_logged_in
 def page3() -> str:
     return 'This is page 3.'
 
