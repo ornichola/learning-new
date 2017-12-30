a2b1049341c597ea7ce1b585b4e12a77acadb923
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-30 17:11:56 +0300

    head-first-python ch10 quick_session.py

---
+++ head-first-python/quick_session.py
@@ -0,0 +1,17 @@
+from flask import Flask, session
+
+app = Flask(__name__)
+
+app.secret_key = 'YouWillNeverGuess'
+
+@app.route('/secret/<user>')
+def setuser(user: str) -> str:
+    session['user'] = user
+    return 'User value set to: ' + session['user']
+
+@app.route('/getuser')
+def getuser() -> str:
+    return 'User values is currently set to: ' + session['user']
+
+if __name__ == '__main__':
+    app.run(debug=True)
