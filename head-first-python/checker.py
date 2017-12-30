6f20a9d53b22a786b13e58e6f04bbd0976a515fd
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-31 01:08:11 +0300

    head-first-python ch10 checker.py fix

--- head-first-python/checker.py
+++ head-first-python/checker.py
@@ -1,7 +1,8 @@
 from functools import wraps
 from flask import session
 
-def check_logged_in(func: object) -> object:
+def check_logged_in(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         if 'logged_in' in session:
             return func(*args, **kwargs)
