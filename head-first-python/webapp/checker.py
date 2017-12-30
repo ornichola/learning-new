35bc51447754da6618568d94195e979c232ec80e
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-31 01:25:19 +0300

    head-first-python ch10 adding checker.py module with decorator to the webapp

---
+++ head-first-python/webapp/checker.py
@@ -0,0 +1,10 @@
+from functools import wraps
+from flask import session
+
+def check_logged_in(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'logged_in' in session:
+            return func(*args, **kwargs)
+        return 'You are NOT logged in.'
+    return wrapper
