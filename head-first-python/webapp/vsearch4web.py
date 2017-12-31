c49db6145b05b7ac2b1ce32372934cd158bb9ea1
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-31 16:29:37 +0300

    head-first-python ch11 webapp/vsearch4web.py view_the_log() is now handling errors

--- head-first-python/webapp/vsearch4web.py
+++ head-first-python/webapp/vsearch4web.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError
 from checker import check_logged_in
-
 
 app = Flask(__name__)
 app.config['dbconfig'] = { 'host': '127.0.0.1',
@@ -54,15 +53,21 @@ def entry_page() -> 'html':
 @check_logged_in
 def view_the_log() -> 'html':
     """Отображает содержимое лога в качестве HTML таблицы"""
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html',
-                            the_title='View Log',
-                            the_row_titles=titles,
-                            the_data=contents,)
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                                the_title='View Log',
+                                the_row_titles=titles,
+                                the_data=contents,)
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', srt(err)))
+    except Exception as err:
+        prtint('Something went wrong:', str(err))
+    return 'Error'
 
 
 @app.route('/login')
