681ae6688e0022b10f8567c1b384a6a400d08cae
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-31 16:52:45 +0300

    head-first-python ch11 webapp/DBcm.py is now handling Credential and SQL errors

--- head-first-python/webapp/DBcm.py
+++ head-first-python/webapp/DBcm.py
@@ -3,6 +3,12 @@ import mysql.connector
 class ConnectionError(Exception):
     pass
 
+class CredentialError(Exception):
+    pass
+
+class SQLError(Exception):
+    pass
+
 class UseDatabase:
 
     def __init__(self, config: dict) -> None:
@@ -13,11 +19,17 @@ class UseDatabase:
             self.conn = mysql.connector.connect(**self.configuraion)
             self.cursor = self.conn.cursor()
             return self.cursor
-        except ConnectionError as err:
+        except mysql.connector.errors.InterfaceError as err:
             raise ConnectionError(err)
+        except mysql.connector.errors.ProgrammingError as err:
+            raise CredentialError(err)
 
 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise SQLError(exc_value)
+        elif exc_type:
+            raise exc_type(exc_value)
