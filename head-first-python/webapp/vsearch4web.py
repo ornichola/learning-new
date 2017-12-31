35bcf31741dc261411c01d3d70baa4fa0eaaf27c
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-31 17:38:00 +0300

    head-first-python ch11-3/4 webapp/vsearch4web.py is now using threading

--- head-first-python/webapp/vsearch4web.py
+++ head-first-python/webapp/vsearch4web.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, escape, session
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
 from vsearch import search4letters
 from DBcm import UseDatabase, ConnectionError, CredentialError, SQLError
 from checker import check_logged_in
+from time import sleep
+from threading import Thread
 
 app = Flask(__name__)
 app.config['dbconfig'] = { 'host': '127.0.0.1',
@@ -11,28 +13,32 @@ app.config['dbconfig'] = { 'host': '127.0.0.1',
 
 app.secret_key = 'YouWillNeverGuess'
 
-def log_request(req: 'flask_request', res: str) -> None:
-    """Журналирует веб-запрос и возвращаемые результаты."""
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-                (phrase, letters, ip, browser_string, results)
-                values
-                (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'],
-                            req.form['letters'],
-                            req.remote_addr,
-                            req.user_agent.browser,
-                            res, ))
-
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """Журналирует веб-запрос и возвращаемые результаты."""
+        sleep(15) # This makes log request really slow...
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """insert into log
+                    (phrase, letters, ip, browser_string, results)
+                    values
+                    (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form['phrase'],
+                                req.form['letters'],
+                                req.remote_addr,
+                                req.user_agent.browser,
+                                res, ))
+
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results'
     results=str(search4letters(phrase, letters))
     try:
-        log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
     except Exception as err:
         print('Some error occured:', str(err))
     return render_template('results.html',
@@ -68,7 +74,7 @@ def view_the_log() -> 'html':
     except CredentialError as err:
         print('User-id/Password issues. Error:', srt(err))
     except SQLError as err:
-        print('Is your query correct? Error:', srt(err))    
+        print('Is your query correct? Error:', srt(err))
     except Exception as err:
         prtint('Something went wrong:', str(err))
     return 'Error'
