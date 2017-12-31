2b3b333cbf1ce20be03917ee320666ad364f995a
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-31 16:29:04 +0300

    head-first-python ch11 webapp/DBcm.py is now handling errors

--- head-first-python/webapp/DBcm.py
+++ head-first-python/webapp/DBcm.py
@@ -1,14 +1,21 @@
 import mysql.connector
 
+class ConnectionError(Exception):
+    pass
+
 class UseDatabase:
 
     def __init__(self, config: dict) -> None:
         self.configuraion = config
 
     def __enter__(self) -> 'cursor':
-        self.conn = mysql.connector.connect(**self.configuraion)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = mysql.connector.connect(**self.configuraion)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except ConnectionError as err:
+            raise ConnectionError(err)
+
 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
         self.conn.commit()
