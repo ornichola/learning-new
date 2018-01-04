4a3b833109034ba031af8be65bf85003a0d68c62
ornichola <1502708+ornichola@users.noreply.github.com>
2018-01-05 01:09:40 +0300

    test polygon doctest example

--- polygon/example-doctest.py
+++ polygon/example-doctest.py
@@ -1,61 +1,38 @@
-"""
-Это модуль-пример.
+'''
+It is a test block, let's try something.
 
-Этот модуль предоставляет одну функцию - factorial().  Например,
+>>> print(quick_math(3, 2, '+'))
+5.0
 
->>> factorial(5)
-120
-"""
+>>> print(quick_math(1, 3, '-'))
+-2.0
 
-def factorial(n):
-    """Возвращает факториал числа n, которое является числом >= 0.
+>>> print(quick_math(5, 10, '*'))
+50.0
 
-    Если резульатат умещается в int, возвращается int.
-    Иначе возвращается long.
+>>> print(quick_math(6, 4, '/'))
+1.5
 
-    >>> [factorial(n) for n in range(6)]
-    [1, 1, 2, 6, 24, 120]
-    >>> [factorial(long(n)) for n in range(6)]
-    [1, 1, 2, 6, 24, 120]
-    >>> factorial(30)
-    265252859812191058636308480000000L
-    >>> factorial(30L)
-    265252859812191058636308480000000L
-    >>> factorial(-1)
-    Traceback (most recent call last):
-        ...
-    ValueError: n must be >= 0
+>>> print(quick_math(2, 6, '**'))
+64.0
 
-    Можно вычислять факториал числа с десятичной частью, если она
-    равна 0:
-    >>> factorial(30.1)
-    Traceback (most recent call last):
-        ...
-    ValueError: n must be exact integer
-    >>> factorial(30.0)
-    265252859812191058636308480000000L
+>>> print(quick_math(3, 2, 'unrecognizable'))
+Cannot recognize operation
+'''
 
-    Кроме того, число не должно быть слишком большим:
-    >>> factorial(1e100)
-    Traceback (most recent call last):
-        ...
-    OverflowError: n too large
-    """
-
-    import math
-    if not n >= 0:
-        raise ValueError("n must be >= 0")
-    if math.floor(n) != n:
-        raise ValueError("n must be exact integer")
-    if n+1 == n:  # перехватываем значения типа 1e300
-        raise OverflowError("n too large")
-    result = 1
-    factor = 2
-    while factor <= n:
-        result *= factor
-        factor += 1
-    return result
-
+def quick_math(a: float, b: float, oper: str) -> float:
+    if oper == '+':
+        return float(a + b)
+    elif oper == '-':
+        return float(a - b)
+    elif oper == '*':
+        return float(a * b)
+    elif oper == '/':
+        return float(a / b)
+    elif oper == '**':
+        return float(a ** b)
+    else:
+        return "Cannot recognize operation"
 
 if __name__ == "__main__":
     import doctest
