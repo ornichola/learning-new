edff08ec7c828af17c7daae664bc707b5e2af636
ornichola <1502708+ornichola@users.noreply.github.com>
2018-01-05 00:55:43 +0300

    test polygon for small scripts

---
+++ polygon/example-doctest.py
@@ -0,0 +1,62 @@
+"""
+Это модуль-пример.
+
+Этот модуль предоставляет одну функцию - factorial().  Например,
+
+>>> factorial(5)
+120
+"""
+
+def factorial(n):
+    """Возвращает факториал числа n, которое является числом >= 0.
+
+    Если резульатат умещается в int, возвращается int.
+    Иначе возвращается long.
+
+    >>> [factorial(n) for n in range(6)]
+    [1, 1, 2, 6, 24, 120]
+    >>> [factorial(long(n)) for n in range(6)]
+    [1, 1, 2, 6, 24, 120]
+    >>> factorial(30)
+    265252859812191058636308480000000L
+    >>> factorial(30L)
+    265252859812191058636308480000000L
+    >>> factorial(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be >= 0
+
+    Можно вычислять факториал числа с десятичной частью, если она
+    равна 0:
+    >>> factorial(30.1)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be exact integer
+    >>> factorial(30.0)
+    265252859812191058636308480000000L
+
+    Кроме того, число не должно быть слишком большим:
+    >>> factorial(1e100)
+    Traceback (most recent call last):
+        ...
+    OverflowError: n too large
+    """
+
+    import math
+    if not n >= 0:
+        raise ValueError("n must be >= 0")
+    if math.floor(n) != n:
+        raise ValueError("n must be exact integer")
+    if n+1 == n:  # перехватываем значения типа 1e300
+        raise OverflowError("n too large")
+    result = 1
+    factor = 2
+    while factor <= n:
+        result *= factor
+        factor += 1
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
