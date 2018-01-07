8b9b568447fa0e9c8e2e78c750c71229b9212108
ornichola <1502708+ornichola@users.noreply.github.com>
2018-01-07 14:52:45 +0300

    test polygon extend test_runner.py

--- polygon/test_runner.py
+++ polygon/test_runner.py
@@ -1,8 +1,10 @@
 import unittest
-import test_calc_unittest
+import test_calc_unittest_03
 
 calcTestSuite = unittest.TestSuite()
-calcTestSuite.addTest(unittest.makeSuite(test_calc_unittest.CalcTest))
+calcTestSuite.addTest(unittest.makeSuite(test_calc_unittest_03.CalcBasicTests))
+calcTestSuite.addTest(unittest.makeSuite(test_calc_unittest_03.CalcExtendedTests))
+print("count of test: " + str(calcTestSuite.countTestCases()) + '\n')
 
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(calcTestSuite)
