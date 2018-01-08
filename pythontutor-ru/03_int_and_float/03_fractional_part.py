d756d06bc5a283daa89d0c00c3a99a86d3ee3bdc
ornichola <1502708+ornichola@users.noreply.github.com>
2018-01-09 01:47:32 +0300

    pythontutor.ru ch03 ex03

---
+++ pythontutor-ru/03_int_and_float/03_fractional_part.py
@@ -0,0 +1,10 @@
+'''
+http://pythontutor.ru/lessons/int_and_float/problems/fractional_part/
+Дано положительное действительное число X. Выведите его дробную часть.
+'''
+
+num = float(input())
+
+print(num % 1)
+#print(num - int(x))
+#print(num - num // 1)
