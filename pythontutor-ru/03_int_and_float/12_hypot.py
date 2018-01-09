5073b588edbf101d55ca89b6e33795007fa77067
ornichola <1502708+ornichola@users.noreply.github.com>
2018-01-09 21:10:03 +0300

    pythontutor.ru ch03 ex12

---
+++ pythontutor-ru/03_int_and_float/12_hypot.py
@@ -0,0 +1,9 @@
+'''
+http://pythontutor.ru/lessons/int_and_float/problems/hypot/
+Дано два числа a и b. Выведите гипотенузу треугольника с заданными катетами.
+'''
+
+a = int(input())
+b = int(input())
+
+print((a**2 + b**2) ** 0.5)
