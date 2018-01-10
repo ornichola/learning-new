921f812fee42a57de041443021a07bdae117139e
ornichola <1502708+ornichola@users.noreply.github.com>
2018-01-10 15:40:59 +0300

    oop __repr__ and __str__

--- oop/Employee.py
+++ oop/Employee.py
@@ -14,63 +14,20 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
 
-class Developer(Employee):
-    raise_amount = 1.10
-
-    def __init__(self, first, last, pay, prog_lang):
-        super().__init__(first, last, pay)
-        #Employee.__init__(self, first, last, pay)
-        self.prog_lang = prog_lang
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
 
 
-class Manager(Employee):
+emp_1 = Employee('Niko', 'Learner', 120000)
+emp_2 = Employee('John', 'Smith', 100000)
 
-    def __init__(self, first, last, pay, employees=None):
-        super().__init__(first, last, pay)
-        if employees is None:
-            self.employees = []
-        else:
-            self.employees = employees
-
-    def add_emp(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
-
-    def remove_emp(self, emp):
-        if emp in self.employees:
-            self.employees.remove(emp)
-
-    def print_emps(self):
-        for emp in self.employees:
-            print('-->', emp.fullname())
-
-
-dev_1 = Developer('Niko', 'Learner', 120000, 'Python')
-dev_2 = Employee('John', 'Smith', 100000)
-
-mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
-
-# Allow to check method resoltion order and other helpful info
-# print(help(Developer))
-
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
+print(emp_1)
 print()
-
-print(dev_1.email)
-print(dev_1.prog_lang)
+print(repr(emp_1))
+print(str(emp_1))
 print()
-
-print(mgr_1.email)
-mgr_1.print_emps()
-mgr_1.add_emp(dev_2)
-mgr_1.print_emps()
-mgr_1.remove_emp(dev_2)
-mgr_1.print_emps()
-print()
-
-print(isinstance(mgr_1, Manager))
-print(isinstance(mgr_1, Employee))
-print(isinstance(mgr_1, Developer))
+print(emp_1.__repr__())
+print(emp_1.__str__())
