23bd6e421283b3fd2710099c1e8cef35f7c61e67
ornichola <1502708+ornichola@users.noreply.github.com>
2018-01-10 15:05:14 +0300

    oop inheritance

--- oop/Employee.py
+++ oop/Employee.py
@@ -1,6 +1,5 @@
 class Employee:
 
-    nums_of_emps = 0
     raise_amount = 1.04
 
     def __init__(self, first, last, pay):
@@ -8,58 +7,70 @@ class Employee:
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
-        Employee.nums_of_emps += 1
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * Employee.raise_amount)
-
-    @classmethod
-    def set_raise_amount(cls, amount):
-        cls.raise_amount = amount
-
-    @classmethod
-    def from_string(cls, emp_str):
-        first, last, pay = emp_str.split('-')
-        return cls(first, last, pay)
-
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
+        self.pay = int(self.pay * self.raise_amount)
 
 
-emp_1 = Employee('Niko', 'Learner', 120000)
-emp_2 = Employee('John', 'Smith', 100000)
+class Developer(Employee):
+    raise_amount = 1.10
 
-Employee.set_raise_amount(1.05)
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        #Employee.__init__(self, first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Niko', 'Learner', 120000, 'Python')
+dev_2 = Employee('John', 'Smith', 100000)
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+# Allow to check method resoltion order and other helpful info
+# print(help(Developer))
+
+print(dev_1.pay)
+dev_1.apply_raise()
+print(dev_1.pay)
 print()
 
-emp_1.set_raise_amount(1.06)
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+print(dev_1.email)
+print(dev_1.prog_lang)
 print()
 
-emp_str_1 = 'John-Doe-70000'
-emp_str_2 = 'Jane-Doe-90000'
-emp_str_3 = 'Steve-Fisher-80000'
+print(mgr_1.email)
+mgr_1.print_emps()
+mgr_1.add_emp(dev_2)
+mgr_1.print_emps()
+mgr_1.remove_emp(dev_2)
+mgr_1.print_emps()
+print()
 
-# Creating new instance using default constructor
-first, last, pay = emp_str_1.split('-')
-new_emp_1 = Employee(first, last, pay)
-print(new_emp_1.fullname())
-
-# Creating new instance using @classmethod constructor
-new_emp_2 = Employee.from_string(emp_str_2)
-print(new_emp_2.fullname())
-
-import datetime
-my_date = datetime.date(2018, 7, 10)
-print(Employee.is_workday(my_date))
+print(isinstance(mgr_1, Manager))
+print(isinstance(mgr_1, Employee))
+print(isinstance(mgr_1, Developer))
