afa12e49e7888f95e21592e5e4cc497c35fed20a
ornichola <1502708+ornichola@users.noreply.github.com>
2018-01-10 14:03:19 +0300

    oop @classmethod and @staticmethod

--- oop/Employee.py
+++ oop/Employee.py
@@ -16,35 +16,50 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * Employee.raise_amount)
 
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
 
-print(Employee.nums_of_emps)
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
 
 emp_1 = Employee('Niko', 'Learner', 120000)
 emp_2 = Employee('John', 'Smith', 100000)
 
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
-
+Employee.set_raise_amount(1.05)
 print(Employee.raise_amount)
 print(emp_1.raise_amount)
 print(emp_2.raise_amount)
+print()
 
-# This statement changes variable for
-# class and ALL instances
-Employee.raise_amount = 1.05
+emp_1.set_raise_amount(1.06)
 print(Employee.raise_amount)
 print(emp_1.raise_amount)
 print(emp_2.raise_amount)
+print()
 
-# This statement changes variable for
-# instance ONLY
-emp_1.raise_amount = 1.06
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Jane-Doe-90000'
+emp_str_3 = 'Steve-Fisher-80000'
 
-print(Employee.__dict__)
-print(emp_1.__dict__)
+# Creating new instance using default constructor
+first, last, pay = emp_str_1.split('-')
+new_emp_1 = Employee(first, last, pay)
+print(new_emp_1.fullname())
 
-print(Employee.nums_of_emps)
+# Creating new instance using @classmethod constructor
+new_emp_2 = Employee.from_string(emp_str_2)
+print(new_emp_2.fullname())
+
+import datetime
+my_date = datetime.date(2018, 7, 10)
+print(Employee.is_workday(my_date))
