32822baab920bd4f858e79473333f0eea4d109e2
ornichola <1502708+ornichola@users.noreply.github.com>
2018-01-10 16:29:28 +0300

    oop adding property setter and deleter

--- oop/Employee.py
+++ oop/Employee.py
@@ -3,12 +3,6 @@ class Employee:
     def __init__(self, first, last):
         self.first = first
         self.last = last
-        #self.email = first + '.' + last + '@company.com'
-
-    # removing instance variable emil and
-    # replacing it with email() method with
-    # decorated by @property will allow us
-    # access email as attribute
 
     @property
     def email(self):
@@ -18,11 +12,29 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
+
 
 emp_1 = Employee('Niko', 'Learner')
 
-emp_1.first = 'Jim'
+emp_1.fullname = 'John Doe'
 
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
+print()
+
+del emp_1.fullname
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname)
