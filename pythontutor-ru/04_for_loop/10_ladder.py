c71d5d123cd0cb0494221ace3e7ed2d22398bb8f
ornichola <1502708+ornichola@users.noreply.github.com>
2018-03-15 22:08:16 +0300

    pythontutor.ru ch04 ex10

---
+++ pythontutor-ru/04_for_loop/10_ladder.py
@@ -0,0 +1,11 @@
+'''
+http://pythontutor.ru/lessons/for_loop/problems/ladder/
+По данному натуральному n ≤ 9 выведите лесенку из n ступенек,
+i-я ступенька состоит из чисел от 1 до i без пробелов.
+'''
+
+n = int(input())
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j, end='')
+    print()
