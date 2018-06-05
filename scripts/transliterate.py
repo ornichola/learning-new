5bed474e1eea5ac514deb1ab42c8004796e06f2c
ornichola <1502708+ornichola@users.noreply.github.com>
2018-06-05 17:04:53 +0300

    script: transliteration from cyrillic to latin alphabet

---
+++ scripts/transliterate.py
@@ -0,0 +1,41 @@
+d = {
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'g',
+    'д': 'd',
+    'е': 'e',
+    'ё': 'e',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'i',
+    'й': 'i',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'kh',
+    'ц': 'ts',
+    'ч': 'ch',
+    'ш': 'sh',
+    'щ': 'shch',
+    'ъ': 'ie',
+    'ы': 'y',
+    'ь': None,
+    'э': 'e',
+    'ю': 'iu',
+    'я': 'ia',
+}
+
+def transliterate(string):
+    translited = ''
+    for ch in string.lower():
+        translited += d.get(ch, ch)
+    return translited
