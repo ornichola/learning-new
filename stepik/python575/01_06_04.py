a497650176a008e3f8549c2112a8c0ca466d83f0
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-14 22:07:34 +0300

    stepik - course 575 - 01 06 04

---
+++ stepik/python575/01_06_04.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_tag_name('input')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name('last_name')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name('city')
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element_by_id('country')
+    input4.send_keys("Russia")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
