b7ee9ae40dec0446893c60fbc2429431cb819a6c
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-14 22:10:40 +0300

    stepik - course 575 - 01 06 07

---
+++ stepik/python575/01_06_07.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import time
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/huge_form.html")
+    elements = browser.find_elements_by_xpath('//input')
+    for element in elements:
+        element.send_keys("Мой ответ")
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
