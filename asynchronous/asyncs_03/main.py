24ce662056c57eb7b40ba6c579e56d2f8767d22e
ornichola <1502708+ornichola@users.noreply.github.com>
2021-08-17 09:39:40 +0300

    Add None replacement for empty line

--- asynchronous/asyncs_03/main.py
+++ asynchronous/asyncs_03/main.py
@@ -1,8 +1,18 @@
+"""
+Два текстовых файла
+Вывести на экран их содержимое в соответствующих строках в виде кортежей
+Если в каком-то файле не хватает строк - выводить None
+
+"""
+
+from itertools import zip_longest
+
+
 def f_reader(f_name: str):
     for row in open(f_name, 'r'):
         row = row.replace('\n', '')
         yield row if row else None
 
 
-for tpl in zip(f_reader('sample_01.txt'), f_reader('sample_02.txt')):
+for tpl in zip_longest(f_reader('sample_01.txt'), f_reader('sample_02.txt')):
     print(tpl)
