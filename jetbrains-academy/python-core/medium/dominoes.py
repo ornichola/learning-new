388e7a648cc6b0ef245b50bc13f58b11c09147c1
ornichola <1502708+ornichola@users.noreply.github.com>
2022-05-13 00:33:58 +0300

    Add JetBrains Academy Python Core dominoes stage 4

--- jetbrains-academy/python-core/medium/dominoes.py
+++ jetbrains-academy/python-core/medium/dominoes.py
@@ -1,164 +1,155 @@
 import random
 
 
-DOMINOES = list()
-PIECES_COMPUTER = list()
-PIECES_PLAYER = list()
-LINE = list()
-TURN = None
-WINNER = None
+class DominoesGame:
+    def __init__(self):
+        self.dominoes = list()
+        self.pieces_computer = list()
+        self.pieces_player = list()
+        self.line = list()
+        self.turn = None
+        self.winner = None
 
+    def form_dominoes(self):
+        self.dominoes = [[i, j] for i in range(7) for j in range(i, 7)]
 
-def form_dominoes() -> [[int]]:
-    n = 7
-    return [[i, j] for i in range(n) for j in range(i, n)]
+    def handle_players(self):
+        self.pieces_computer = [self.dominoes.pop(self.dominoes.index(random.choice(self.dominoes))) for _ in range(7)]
+        self.pieces_player = [self.dominoes.pop(self.dominoes.index(random.choice(self.dominoes))) for _ in range(7)]
 
+    def switch_turn(self):
+        self.turn = 'computer' if self.turn == 'player' else 'player'
 
-def handle_pieces():
-    max_per_player = 7
-    global DOMINOES, PIECES_COMPUTER, PIECES_PLAYER
-    DOMINOES = form_dominoes()
-    PIECES_COMPUTER = [DOMINOES.pop(DOMINOES.index(random.choice(DOMINOES))) for _ in range(max_per_player)]
-    PIECES_PLAYER = [DOMINOES.pop(DOMINOES.index(random.choice(DOMINOES))) for _ in range(max_per_player)]
+    def setup_table(self):
+        # handling dominoes on players hand until biggest equal can be placed
+        starting_domino, biggest_value = None, 0
+        while not starting_domino:
+            self.form_dominoes()
+            self.handle_players()
+            chose_from = self.pieces_computer + self.pieces_player
+            for domino in chose_from:
+                if domino[0] == domino[1] and sum(domino) > biggest_value:
+                    starting_domino, biggest_value = domino, sum(domino)
 
-
-def setup_table():
-    global DOMINOES, PIECES_COMPUTER, PIECES_PLAYER, LINE, TURN
-    starting_domino, biggest_value = None, 0
-    while not starting_domino:
-        handle_pieces()
-        chose_from = PIECES_COMPUTER + PIECES_PLAYER
-        for domino in chose_from:
-            if domino[0] == domino[1] and sum(domino) > biggest_value:
-                starting_domino, biggest_value = domino, sum(domino)
-        if not starting_domino:
-            continue
+        # making first move
+        if starting_domino in self.pieces_computer:
+            self.pieces_computer.remove(starting_domino)
+            self.turn = 'player'
         else:
-            if starting_domino in PIECES_COMPUTER:
-                PIECES_COMPUTER.remove(starting_domino)
-                TURN = 'player'
-            else:
-                PIECES_PLAYER.remove(starting_domino)
-                TURN = 'computer'
+            self.pieces_player.remove(starting_domino)
+            self.turn = 'computer'
 
-            LINE.append(starting_domino)
+        self.line.append(starting_domino)
 
+    def draw_interface(self):
+        print('======================================================================')
+        print(f'Stock size: {len(self.dominoes)}')
+        print(f'Computer pieces: {len(self.pieces_computer)}')
+        if len(self.line) < 7:
+            print(f'\n{"".join(str(d) for d in self.line)}\n')
+        else:
+            print(f'\n{"".join(str(d) for d in self.line[0:3])}...{"".join(str(d) for d in self.line[-3:])}\n')
+        print('Your pieces:')
+        for n, p in enumerate(self.pieces_player, start=1):
+            print(f'{n}:{p}')
+        if self.turn == 'computer':
+            print('\nStatus: Computer is about to make a move. Press Enter to continue...')
+        else:
+            print('\nStatus: It\'s your turn to make a move. Enter your command.')
 
-def draw_interface():
-    print('======================================================================')
-    print(f'Stock size: {len(DOMINOES)}')
-    print(f'Computer pieces: {len(PIECES_COMPUTER)}')
-    if len(LINE) < 7:
-        print(f'\n{"".join(str(d) for d in LINE)}\n')
-    else:
-        print(f'{"".join(str(d) for d in LINE[0:3])}...{"".join(str(d) for d in LINE[-3:-1])}')
-    print('Your pieces:')
-    for n, p in enumerate(PIECES_PLAYER, start=1):
-        print(f'{n}:{p}')
-    if TURN == 'computer':
-        print('\nStatus: Computer is about to make a move. Press Enter to continue...')
-    else:
-        print('\nStatus: It\'s your turn to make a move. Enter your command.')
+    def check_winner(self):
+        if not self.pieces_player:
+            self.winner = 'player'
+        elif not self.pieces_computer:
+            self.winner = 'computer'
+        else:
+            snake = [n for d in self.line for n in d]
+            if snake[0] == snake[-1]:
+                if snake.count(snake[0]) == 8:
+                    self.winner = 'draw'
 
+    def print_winner(self):
+        if self.winner == 'player':
+            print('Status: The game is over. You won!')
+        elif self.winner == 'computer':
+            print('Status: The game is over. The computer won!')
+        elif self.winner == 'draw':
+            print('Status: The game is over. It\'s a draw!')
+        else:
+            print('Something went completely wrong')
 
-def _handle_user_input():
-    while True:
-        try:
-            domino_index = int(input())
-            if abs(domino_index) not in range(0, len(PIECES_PLAYER) + 1):
-                raise ValueError
-            break
-        except ValueError:
-            print('Invalid input. Please try again.')
-            continue
-    return domino_index
-
-
-def start_game_loop():
-    global TURN, WINNER
-    while True:
-        if TURN == 'player':
-            player_domino_index = _handle_user_input()
-            if player_domino_index == 0:
-                if DOMINOES:
-                    PIECES_PLAYER.append(DOMINOES.pop(DOMINOES.index(random.choice(DOMINOES))))
-                    TURN = 'computer'
-                else:
-                    continue
-            else:
-                if PIECES_PLAYER:
-                    inserting_domino = PIECES_PLAYER.pop(abs(player_domino_index) - 1)
-
-                    # processing move availability
-                    border_domino = LINE[-1] if player_domino_index > 0 else LINE[0]
-                    border = border_domino[-1] if player_domino_index > 0 else border_domino[0]
-                    if any(b == border for b in inserting_domino):
-                        if player_domino_index < 0 and inserting_domino[0] == border:
-                            inserting_domino.reverse()
-                        if player_domino_index > 0 and inserting_domino[1] == border:
-                            inserting_domino.reverse()
-                    else:
-                        print('Illegal move. Please try again.')
-                        continue
-
-                    if player_domino_index > 0:
-                        LINE.append(inserting_domino)
-                    else:
-                        LINE.insert(0, inserting_domino)
-                    TURN = 'computer'
-                else:
-                    WINNER = TURN
+    def make_player_move(self):
+        while True:
+            while True:
+                try:
+                    domino_index = int(input())
+                    if abs(domino_index) not in range(0, len(self.pieces_player) + 1):
+                        raise ValueError
                     break
-        else:
-            computer_size = len(PIECES_COMPUTER)
-            if computer_size:
-                input()
-                TURN = 'player'
+                except ValueError:
+                    print('Invalid input. Please try again.')
+                    continue
+            if domino_index != 0:
+                index = abs(domino_index) - 1
+                domino = self.pieces_player.pop(index)
 
-                # handling computer move
-                borders = {'left': LINE[0][0], 'right': LINE[-1][-1]}
-                for domino in PIECES_COMPUTER:
-                    if domino[0] == borders['left']:
-                        domino.reverse()
-                        LINE.insert(0, domino)
-                        break
-                    elif domino[1] == borders['left']:
-                        LINE.insert(0, domino)
-                        break
-                    elif domino[0] == borders['right']:
-                        LINE.append(domino)
-                        break
-                    elif domino[1] == borders['right']:
-                        domino.reverse()
-                        LINE.append(domino)
-                        break
-                else:
-                    if DOMINOES:
-                        PIECES_COMPUTER.append(DOMINOES.pop(DOMINOES.index(random.choice(DOMINOES))))
+                # processing move availability
+                border = self.line[-1][-1] if domino_index > 0 else self.line[0][0]
+                if domino[0] != border and domino[1] != border:
+                    self.pieces_player.insert(index, domino)
+                    print('Illegal move. Please try again.')
+                    continue
+
+                if domino[0] == border and domino_index < 0 or domino_index > 0 and domino[1] == border:
+                    domino.reverse()
+
+                self.line.append(domino) if domino_index > 0 else self.line.insert(0, domino)
+                self.switch_turn()
+                break
             else:
-                WINNER = TURN
+                if self.dominoes:
+                    self.pieces_player.append(
+                        self.dominoes.pop(self.dominoes.index(random.choice(self.dominoes))))
+                    self.switch_turn()
                 break
 
-        # check draw condition
-        snake = [n for d in LINE for n in d]
-        if snake[0] == snake[-1]:
-            if snake.count(snake[0]) == 8:
-                WINNER = 'draw'
+    def make_computer_move(self):
+        input()
+        self.switch_turn()
+
+        # handling computer move
+        borders = {'left': self.line[0][0], 'right': self.line[-1][-1]}
+        for choice in self.pieces_computer:
+            if any(b in borders.values() for b in choice):
+                if choice[0] == borders['left'] or choice[1] == borders['left']:
+                    if choice[0] == borders['left']:
+                        choice.reverse()
+                    self.line.insert(0, choice)
+                    self.pieces_computer.remove(choice)
+                else:
+                    if choice[1] == borders['right']:
+                        choice.reverse()
+                    self.line.append(choice)
+                    self.pieces_computer.remove(choice)
                 break
+        else:
+            if self.dominoes:
+                self.pieces_computer.append(self.dominoes.pop(self.dominoes.index(random.choice(self.dominoes))))
 
-        draw_interface()
+    def game_loop(self):
+        while not self.winner:
+            if self.turn == 'player':
+                self.make_player_move()
+            else:
+                self.make_computer_move()
+            self.check_winner()
+            self.draw_interface()
+
+    def play(self):
+        self.setup_table()
+        self.draw_interface()
+        self.game_loop()
+        self.print_winner()
 
 
-setup_table()
-draw_interface()
-start_game_loop()
-
-# print out winner
-if WINNER == 'player':
-    print('Status: The game is over. You won!')
-elif WINNER == 'computer':
-    print('Status: The game is over. The computer won!')
-elif WINNER == 'draw':
-    print('Status: The game is over. It\'s a draw!')
-else:
-    print('Something went completely wrong')
+DominoesGame().play()
