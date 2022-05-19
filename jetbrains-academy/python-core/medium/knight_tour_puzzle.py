5c953ad5f76fb2feb91fda058eafc34b827b2116
ornichola <1502708+ornichola@users.noreply.github.com>
2022-05-19 12:37:37 +0300

    Add JetBrains Academy Python Core knight's tour puzzle stage 2

--- jetbrains-academy/python-core/medium/knight_tour_puzzle.py
+++ jetbrains-academy/python-core/medium/knight_tour_puzzle.py
@@ -1,59 +1,51 @@
-N = 8
-RANGE = range(1, N + 1)  # 1, 2, 3, 4, 5, 6, 7, 8
-DASH = ' -------------------'
-
-# ***********
-# DEBUG
-
-# default, upper-left is (0, 0), bottom-right is (7, 7)
-# chess_board = [[(i, j) for i in range(8)] for j in range(8)]
-
-# Cartesian, bottom-left is (1, 1), upper-right is (8, 8)
-# chess_board = list()
-# for i in range(N):
-#     line = list()
-#     for j in range(N):
-#         line.append((j + 1, N - i))
-#     chess_board.append(line)
-
-# board checker
-# for line in chess_board:
-#     print(line)
-
-# GUBED
-# ***********
-
-
-def form_board(position: list[int] = None):
-    # no position saving currently
-    chess_board = list()
-    for i in range(N):
-        line = list()
-        for j in range(N):
-            line.append('X' if position and [j + 1, N - i] == position else '_')
-        chess_board.append(line)
-
-    return chess_board
-
-
-def draw_board(board: [[str]]):
-    for i, line in zip(range(8, 0, -1), board):
-        print(f'{i}| {" ".join(line)} |')
-
-
-def handle_input():
-    while True:
-        try:
-            inp = [int(i) for i in input('Enter the knight\'s starting position: ').split()]
-            if len(inp) == 2 and all(1 <= i <= 8 for i in inp):
-                return inp
+# reading board dimension in COLUMNS ROWS format
+while True:
+    try:
+        dimensions = [int(i) for i in input('Enter your board dimensions: ').split()]
+        if len(dimensions) != 2 or any(d <= 0 for d in dimensions):
             raise ValueError
-        except ValueError:
-            print('Invalid dimensions!')
+        break
+    except ValueError:
+        print('Invalid dimensions!')
+
+# forming some intermediate data
+cell_size = len(str(dimensions[0] * dimensions[1]))
+border = f'{" " * len(str(dimensions[0]))}{"-" * (dimensions[0] * (cell_size + 1) + 3)}'
+board = [['_' * cell_size for _ in range(dimensions[0])] for _ in range(dimensions[1])]
+
+# reading knight position
+# remember that (1, 1) is a bottom left and (max_column, max_row) is an upper right
+while True:
+    try:
+        position = [int(p) for p in input('Enter the knight\'s starting position: ').split()]
+        if len(position) == 2 and all([1 <= position[0] <= dimensions[0], 1 <= position[1] <= dimensions[1]]):
+            break
+        raise ValueError
+    except ValueError:
+        print('Invalid position!')
+
+# placing knight
+for i in range(dimensions[1]):      # rows      0 1 2 3 4 (5)       for 6 5 input in dimension
+    for j in range(dimensions[0]):  # columns   0 1 2 3 4 5 (6)     for 6 5 input in dimension
+        # this is very fragile solution
+        column_id = j + 1
+        row_id = dimensions[1] - i
+        if [column_id, row_id] == position:
+            # as far as cells represented as '_' * cell_size and strings are immutable
+            cell = list(board[i][j])
+            cell = [' ' for _ in cell]
+            cell[-1] = 'X'
+            board[i][j] = ''.join(cell)
 
 
-pos = handle_input()
-print(DASH)
-draw_board(form_board(pos))
-print(DASH)
-print(f'   {" ".join(str(i) for i in RANGE)} ')
+# printing whole field with placed knight
+print(border)
+for i, line in zip(range(len(board), 0, -1), board):
+    index = f'{" " * (len(str(len(board))) - len(str(i)))}{i}'  # how many spaces should be before each row number
+    print(f'{index}| {" ".join(line)} |')
+print(border)
+
+# printing footer nums
+spaces_before_nums_range = ' ' * (len(str(len(board))) + 2)
+nums_range = ' '.join([f'{" " * (cell_size - len(str(n)))}{str(n)}' for n in range(1, dimensions[0] + 1)])
+print(f'{spaces_before_nums_range}{nums_range}')
