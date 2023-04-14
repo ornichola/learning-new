4ca87b97d8121a9cc946e24cbc68e6269db5341a
ornichola <1502708+ornichola@users.noreply.github.com>
2023-04-14 21:11:15 +0200

    dev-division: hm01: task04

--- dev-division/hm01.py
+++ dev-division/hm01.py
@@ -53,7 +53,47 @@ def task_03(dictionary: dict):
     # print(sorted(dictionary, key=dictionary.get)[-2:])
 
 
+def task_04(lst: list):
+    """
+    Дан произвольный список. Необходимо написать минимум 4 способа развернуть данный список в обратную сторону.
+    Один из способов должен быть реализован самостоятельно, без использования встроенных возможностей списка.
+    """
+    print('original', lst)
+
+    # first
+    r_01 = list(reversed(lst))
+    print('first', r_01)
+
+    # second
+    r_02 = lst[::-1]
+    print('second', r_02)
+
+    # third
+    # lst.reverse()
+    # print('third', lst)
+
+    # fourth
+    r_04, index = list(), -1
+    for _ in range(len(lst)):
+        r_04.append(lst[index])
+        index -= 1
+    print('fourth', r_04)
+
+    # fifth
+    r_05 = list()
+    for i in lst:
+        r_05.insert(0, i)
+    print('fifth', r_05)
+
+    # sixth
+    r_06 = list()
+    for _ in range(len(lst)):
+        r_06.append(lst.pop())
+    print('n', r_06)
+
+
 if __name__ == '__main__':
     # task_01(NUMS)
     # task_02(1234567890)
-    task_03({'a':500, 'b':5874, 'c': 560,'d':400, 'e':5874, 'f': 20})
+    # task_03({'a':500, 'b':5874, 'c': 560,'d':400, 'e':5874, 'f': 20})
+    task_04([1, 4, 6, 9, 2, 3, 5])
