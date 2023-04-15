05fe0c4a49b82d7e20656349719c05c00cf49a2d
ornichola <1502708+ornichola@users.noreply.github.com>
2023-04-16 01:54:47 +0200

    dev-division: hm01: task07

---
+++ dev-division/hm01/task07.py
@@ -0,0 +1,24 @@
+"""
+Дана функция, которая ничего не делает случайное количество времени. Необходимо написать декоратор,
+который считает и выводит на экран время выполнения этой функции.
+"""
+
+import random
+import time
+
+
+def timeit(f):
+    def wrapper(*args, **kwargs):
+        time_start = time.time()
+        f(*args, **kwargs)
+        print(f'Took: {time.time() - time_start}')
+    return wrapper
+
+@timeit
+def random_sleep():
+    time.sleep(random.randint(0, 10))
+
+random_sleep()
+
+# timed = timeit(random_sleep)
+# timed()
